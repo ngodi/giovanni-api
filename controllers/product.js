06a69869b232f50a843250df8c6bfa72c0b3159c
ngodi <albertngdi@gmail.com>
2020-10-27 06:23:38 +0100

    setup orders listing

--- controllers/product.js
+++ controllers/product.js
@@ -84,13 +84,6 @@ exports.update = (req, res) => {
                error: "Image could not be uploaded"
            })
        }
-       const { name, description, price, category, quantity, shipping } = fields
-      
-       if( !name || !description || !price || !category || !quantity || !shipping){
-           return res.status(400).json({
-               error: "All fields are required"
-           })
-       }
 
        let product = req.product
        product = _.extend(product, fields)
